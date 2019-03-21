a2210bbb8ee60636ae7dd63c87c9dc9933922a05
Zames <z5254215560@gmail.com>
2019-03-21 22:32:29 +0800

    Learn build login and logout

--- test/mytestsite/mytestsite/urls.py
+++ test/mytestsite/mytestsite/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from ugo.views import index
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'',index)
+    path('',index),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- test/mytestsite/ugo/views.py
+++ test/mytestsite/ugo/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
-    data = {
-        'foo':{
-            'x': 568
-        }
-    }
-    return render(request,'123.html',data)
+    return render(request,'123.html',locals())
+def login(request):
+
+    if request.user.is_authenticated(): 
+        return HttpResponseRedirect('/index/')
+
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render_to_response('login.html') 
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/index/')
